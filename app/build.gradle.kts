9b621fc464cef0f33c859f47c5cbf1ef5ad839a7
Novikov Pavel <npn90@yandex.ru>
2024-04-07 23:28:51 +0300

    update versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
@@ -53,16 +53,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.13.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0-alpha04")
+    implementation("androidx.activity:activity-ktx:1.9.0-rc01")
+    implementation("androidx.activity:activity-compose:1.9.0-rc01")
+//    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    implementation("androidx.compose.ui:ui:1.7.0-alpha06")
+    implementation("androidx.compose.ui:ui-graphics:1.7.0-alpha06")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.0-alpha06")
+    implementation("androidx.compose.material3:material3:1.3.0-alpha04")
+    implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -71,19 +71,19 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation("androidx.compose.foundation:foundation:1.6.5")
+    implementation("androidx.compose.foundation:foundation:1.7.0-alpha06")
 
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("com.squareup.okhttp3:okhttp")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.8.0-alpha06")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation ("com.google.accompanist:accompanist-navigation-animation:0.28.0")
-    implementation ("com.google.dagger:hilt-android:2.51")
-    kapt  ("com.google.dagger:hilt-compiler:2.51")
+    implementation ("com.google.accompanist:accompanist-navigation-animation:0.35.0-alpha")
+    implementation ("com.google.dagger:hilt-android:2.51.1")
+    kapt  ("com.google.dagger:hilt-compiler:2.51.1")
     implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0-alpha04")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation ("androidx.room:room-runtime:2.6.1")
 
